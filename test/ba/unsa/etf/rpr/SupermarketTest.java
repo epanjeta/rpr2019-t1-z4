a868cbbf8a807d26a227378da7f94c54bc697b66
epanjeta <epanjeta1@etf.unsa.ba>
2020-11-06 12:31:35 +0100

    Dodani testovi za klasu Supermarket

--- test/ba/unsa/etf/rpr/SupermarketTest.java
+++ test/ba/unsa/etf/rpr/SupermarketTest.java
@@ -9,12 +9,23 @@ class SupermarketTest {
     @Test
     void dodajArtikl() {
         Supermarket sp = new Supermarket();
-        Artikl a = new Artikl("Automobil", 15000, "077");
-        sp.dodajArtikl(a);
-        //assertEquals()
+        Artikl a1 = new Artikl("Automobil", 15000, "077");
+        Artikl a2 = new Artikl("Automobil2", 20000, "078");
+        sp.dodajArtikl(a1); sp.dodajArtikl(a2);
+        Artikl[] niz = new Artikl[1000];
+        niz[0] = a1; niz[1] = a2;
+        assertArrayEquals(niz, sp.getArtikli());
     }
 
     @Test
     void izbaciArtiklSaKodom() {
+        Supermarket sp = new Supermarket();
+        Artikl a1 = new Artikl("Automobil", 15000, "077");
+        Artikl a2 = new Artikl("Automobil2", 20000, "078");
+        sp.dodajArtikl(a1); sp.dodajArtikl(a2);
+        Artikl[] niz = new Artikl[1000];
+        niz[0] = a1;
+        sp.izbaciArtiklSaKodom("078");
+        assertArrayEquals(niz, sp.getArtikli());
     }
 }
